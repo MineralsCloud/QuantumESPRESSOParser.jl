84d828aa70da7fe4fd08df24ec102a147a585c39
Qi Zhang <singularitti@outlook.com>
2019-07-31 04:22:02 -0400

    Add inclusions

--- src/QuantumESPRESSOParsers.jl
+++ src/QuantumESPRESSOParsers.jl
@@ -1,6 +1,8 @@
 module QuantumESPRESSOParsers
 
 include("Utils.jl")
+include("BasicIO/BasicIO.jl")
+include("InputParsers/InputParsers.jl")
 include("OutputParsers/OutputParsers.jl")
 
 end # module
