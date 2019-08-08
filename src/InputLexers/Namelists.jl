fa1caa1da270c09e024d625a0efc68ef0c542dc2
Qi Zhang <singularitti@outlook.com>
2019-08-08 00:27:45 -0400

    Add src/InputLexers/Namelists.jl and include

---
+++ src/InputLexers/Namelists.jl
@@ -0,0 +1,18 @@
+"""
+# module Namelists
+
+
+
+# Examples
+
+```jldoctest
+julia>
+```
+"""
+module Namelists
+
+using Reexport
+
+@reexport using Fortran90Namelists.Tokenize
+
+end
--- src/QuantumESPRESSOParsers.jl
+++ src/QuantumESPRESSOParsers.jl
@@ -2,6 +2,7 @@ module QuantumESPRESSOParsers
 
 include("io.jl")
 include("Utils.jl")
+include("InputLexers/InputLexers.jl")
 include("InputParsers/InputParsers.jl")
 include("OutputParsers/OutputParsers.jl")
 
