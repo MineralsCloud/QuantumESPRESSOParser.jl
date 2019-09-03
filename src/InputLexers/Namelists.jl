8292784432836b7acb11b6207a441f2f90d01c23
Qi Zhang <singularitti@outlook.com>
2019-09-02 20:17:19 -0400

    Move `NAMELIST_ITEM_REGEX` to src/InputLexers/Namelists.jl

--- src/InputLexers/Namelists.jl
+++ src/InputLexers/Namelists.jl
@@ -15,8 +15,16 @@ using QuantumESPRESSOParsers
 
 export lexnamelist
 
+# This regular expression is taken from https://github.com/aiidateam/qe-tools/blob/develop/qe_tools/parsers/qeinputparser.py
+const NAMELIST_ITEM_REGEX = r"""
+[ \t]* (\S+?) [ \t]*  # match and store key
+=               # equals sign separates key and value
+[ \t]* (\S+?) [ \t]*  # match and store value
+[\n,]           # return or comma separates "key = value" pairs
+"""mx
+
 function lexnamelist(content::AbstractString)
-    captured = map(x -> x.captures, eachmatch(QuantumESPRESSOParsers.NAMELIST_ITEM_REGEX, content))
+    captured = map(x -> x.captures, eachmatch(NAMELIST_ITEM_REGEX, content))
     dict = Dict{String, Any}()
     for (key, value) in captured
         dict[key] = value
--- src/prelude.jl
+++ src/prelude.jl
@@ -6,10 +6,3 @@ const NAMELIST_BLOCK_REGEX = r"""
 )                        # save the group of text between nmlst
 ^ [ \t]* / [ \t]* $\n    # match line w/ "/" as only non-whitespace char
 """mx
-# This regular expression is taken from https://github.com/aiidateam/qe-tools/blob/develop/qe_tools/parsers/qeinputparser.py
-const NAMELIST_ITEM_REGEX = r"""
-[ \t]* (\S+?) [ \t]*  # match and store key
-=               # equals sign separates key and value
-[ \t]* (\S+?) [ \t]*  # match and store value
-[\n,]           # return or comma separates "key = value" pairs
-"""mx
