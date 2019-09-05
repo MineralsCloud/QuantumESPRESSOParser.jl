dbf97c9f969b49c2f90075ccd383299d9ed112fe
Qi Zhang <singularitti@outlook.com>
2019-09-05 00:20:53 -0400

    Deprecate `InputLexers` module, merge with `InputParsers` module

--- src/InputParsers/Namelists.jl
+++ src/InputParsers/Namelists.jl
@@ -17,7 +17,24 @@ using Fortran90Namelists.FortranToJulia: FortranData
 using QuantumESPRESSOBase.Namelists
 using QuantumESPRESSOBase.Namelists.PWscf
 
-using QuantumESPRESSOParsers.InputLexers.Namelists
+export lexnamelist
+
+# This regular expression is taken from https://github.com/aiidateam/qe-tools/blob/develop/qe_tools/parsers/qeinputparser.py
+const NAMELIST_ITEM_REGEX = r"""
+[ \t]* (\S+?) [ \t]*  # match and store key
+=               # equals sign separates key and value
+[ \t]* (\S+?) [ \t]*  # match and store value
+[\n,]           # return or comma separates "key = value" pairs
+"""mx
+
+function lexnamelist(content::AbstractString)
+    captured = map(x -> x.captures, eachmatch(NAMELIST_ITEM_REGEX, content))
+    dict = Dict{String, Any}()
+    for (key, value) in captured
+        dict[key] = value
+    end
+    return dict
+end # function lexnamelist
 
 function Base.parse(T::Type{<:Namelist}, content::AbstractString)
     regex = r"([\w\d]+)(?:\((\d+)\))?"
--- src/QuantumESPRESSOParsers.jl
+++ src/QuantumESPRESSOParsers.jl
@@ -3,7 +3,6 @@ module QuantumESPRESSOParsers
 include("prelude.jl")
 include("io.jl")
 include("Utils.jl")
-include("InputLexers/InputLexers.jl")
 include("InputParsers/InputParsers.jl")
 include("OutputParsers/OutputParsers.jl")
 
