365b1fb44c173e14b11c768ce943086575de54a6
Qi Zhang <singularitti@outlook.com>
2019-09-05 00:28:08 -0400

    Deprecate io.jl & prelude.jl

--- src/QuantumESPRESSOParsers.jl
+++ src/QuantumESPRESSOParsers.jl
@@ -1,7 +1,5 @@
 module QuantumESPRESSOParsers
 
-include("prelude.jl")
-include("io.jl")
 include("Utils.jl")
 include("InputParsers/InputParsers.jl")
 include("OutputParsers/OutputParsers.jl")
--- src/prelude.jl
+++
@@ -1,8 +0,0 @@
-# This regular expression is taken from https://github.com/aiidateam/qe-tools/blob/develop/qe_tools/parsers/qeinputparser.py
-const NAMELIST_BLOCK_REGEX = r"""
-^ [ \t]* &(\S+) [ \t]* $\n  # match line w/ nmlst tag; save nmlst name
-(
- [\S\s]*?                # match any line non-greedily
-)                        # save the group of text between nmlst
-^ [ \t]* / [ \t]* $\n    # match line w/ "/" as only non-whitespace char
-"""mx
