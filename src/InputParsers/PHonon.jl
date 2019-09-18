3230e205141967105afa6f845297c1c5c72ec360
wjy <15151829012@163.com>
2019-09-18 03:03:28 -0400

    Add `parse` function for q2r input

--- src/InputParsers/PHonon.jl
+++ src/InputParsers/PHonon.jl
@@ -30,6 +30,20 @@ const Q_POINTS_SPECIAL_ITEM_REGEX = r"""
 ^ [ \t]* (\S+) [ \t]+ (\S+) [ \t]+ (\S+) [ \t]+ (\S+) [ \t]* $\n?
 """mx
 
+const Q2R_POINTS_BLOCK_REGEX = r"""
+(?P<block>
+ (?:
+  ^ [ \t]* \S+ [,\t]+ \S+ [,\t]+ \S+ $\n?
+ )+
+)
+^ [ \t]* (\S+) [ \t]* $\n?
+"""imx
+
+const Q2R_POINTS_ITEM_REGEX = r"""
+(^ [ \t]* \S+ [,\t]+ \S+ [,\t]+ \S+ $\n?)
+^ [\t]* (\S+) [\t]*$\n?
+"""mx
+
 function Base.parse(::Type{<:QPointsSpecsCard}, str::AbstractString)
     m = match(Q_POINTS_SPECIAL_BLOCK_REGEX, str)
     if !isnothing(m)
@@ -48,6 +62,22 @@ function Base.parse(::Type{<:QPointsSpecsCard}, str::AbstractString)
     @info "Cannot find card `Q_POINTS`!"
 end # function Base.parse
 
+# function Base.parse(::Type{<:Q2RPointCard}, str::AbstractString)
+#     m = match(Q2R_POINTS_BLOCK_REGEX, str)
+#     if !isnothing(m)
+#         captured = m.captures
+#         data = Q2RPoint[]
+#         matched = match(Q2R_POINTS_ITEM_REGEX, captured)
+#         point = @match map(x -> parse(Float64, FortranData(x)), matched.captures) begin
+#             [grid, nqs] => Q2RPoint(grid, nqs)
+#         end
+#         push!(data, point)
+#         return Q2RPointCard(data)
+#     end
+
+#     @info "Cannot find card `Q_POINTS`!"
+# end # function Base.parse
+
 function Base.parse(::Type{PHononInput}, str::AbstractString)
     inputph = parse(PHNamelist, str)
     q_points = parse(QPointsSpecsCard, str)
@@ -56,7 +86,9 @@ end # function Base.parse
 
 function Base.parse(::Type{Q2RInput}, str::AbstractString)
     input = parse(Q2RNamelist, str)
-    return Q2RInput(input)
+    grid = parse()
+    nqs = parse()
+    return Q2RInput(input,grid,nqs)
 end # function Base.parse
 
 function Base.parse(::Type{MatdynInput}, str::AbstractString)
