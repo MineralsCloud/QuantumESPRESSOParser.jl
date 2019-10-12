dae48df345b84b02fc7cbf7e2e8341eb5730208c
Qi Zhang <singularitti@outlook.com>
2019-10-12 03:48:59 -0400

    Rename regexes in src/InputParsers/Namelists.jl

--- src/InputParsers/Namelists.jl
+++ src/InputParsers/Namelists.jl
@@ -20,23 +20,25 @@ using QuantumESPRESSOBase.Namelists.PWscf
 using QuantumESPRESSOBase.Namelists.PHonon
 
 # This regular expression is taken from https://github.com/aiidateam/qe-tools/blob/develop/qe_tools/parsers/qeinputparser.py
-const NAMELIST_BLOCK_REGEX = r"""
+const NAMELIST_BLOCK =
+r"""
 ^ [ \t]* &(\S+) [ \t]* $\n  # match line w/ nmlst tag; save nmlst name
 (
- [\S\s]*?                # match any line non-greedily
-)                        # save the group of text between nmlst
-^ [ \t]* / [ \t]* $\n    # match line w/ "/" as only non-whitespace char
+    [\S\s]*?                # match any line non-greedily
+)                           # save the group of text between nmlst
+^ [ \t]* / [ \t]* $\n       # match line w/ "/" as only non-whitespace char
 """mx
 # This regular expression is taken from https://github.com/aiidateam/qe-tools/blob/develop/qe_tools/parsers/qeinputparser.py
-const NAMELIST_ITEM_REGEX = r"""
+const NAMELIST_ITEM = 
+r"""
 [ \t]* (\S+?) [ \t]*  # match and store key
-=               # equals sign separates key and value
+=                     # equals sign separates key and value
 [ \t]* (\S+?) [ \t]*  # match and store value
-[\n,]           # return or comma separates "key = value" pairs
+[\n,]                 # return or comma separates "key = value" pairs
 """mx
 
 function findnamelists(str::AbstractString)
-    captured = map(x -> x.captures, eachmatch(NAMELIST_BLOCK_REGEX, str))
+    captured = map(x -> x.captures, eachmatch(NAMELIST_BLOCK, str))
     dict = Dict{Symbol,String}()
     for (name, content) in captured
         @match uppercase(name) begin
@@ -51,7 +53,7 @@ function findnamelists(str::AbstractString)
 end # function findnamelists
 
 function lexnamelist(content::AbstractString)
-    captured = map(x -> x.captures, eachmatch(NAMELIST_ITEM_REGEX, content))
+    captured = map(x -> x.captures, eachmatch(NAMELIST_ITEM, content))
     dict = Dict{String,Any}()
     for (key, value) in captured
         dict[key] = value
