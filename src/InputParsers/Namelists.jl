066a38b113fac543040d41f98d9a95f22a91ba2f
Qi Zhang <singularitti@outlook.com>
2019-10-13 22:56:22 -0400

    refactor: Rewrite src/InputParsers/Namelists.jl

--- src/InputParsers/Namelists.jl
+++ src/InputParsers/Namelists.jl
@@ -19,7 +19,7 @@ using QuantumESPRESSOBase.Namelists
 using QuantumESPRESSOBase.Namelists.PWscf
 using QuantumESPRESSOBase.Namelists.PHonon
 
-# This regular expression is taken from https://github.com/aiidateam/qe-tools/blob/develop/qe_tools/parsers/qeinputparser.py
+# This regular expression is taken from https://github.com/aiidateam/qe-tools/blob/develop/qe_tools/parsers/qeinputparser.py.
 const NAMELIST_BLOCK =
 r"""
 ^ [ \t]* &(\S+) [ \t]* $\n  # match line w/ nmlst tag; save nmlst name
@@ -28,63 +28,56 @@ r"""
 )                           # save the group of text between nmlst
 ^ [ \t]* / [ \t]* $\n       # match line w/ "/" as only non-whitespace char
 """mx
-# This regular expression is taken from https://github.com/aiidateam/qe-tools/blob/develop/qe_tools/parsers/qeinputparser.py
-const NAMELIST_ITEM = 
+# This regular expression is referenced from https://github.com/aiidateam/qe-tools/blob/develop/qe_tools/parsers/qeinputparser.py.
+const NAMELIST_ITEM =
 r"""
-[ \t]* (\S+?) [ \t]*  # match and store key
-=                     # equals sign separates key and value
-[ \t]* (\S+?) [ \t]*  # match and store value
-[\n,]                 # return or comma separates "key = value" pairs
+[ \t]* (?<key> \S+? )(?: (?<kind> [\(%]) (?<index> \w+) \)? )? [ \t]*  # match and store key
+=                              # equals sign separates key and value
+[ \t]* (?<value> \S+?) [ \t]*  # match and store value
+[\n,]                          # return or comma separates "key = value" pairs
 """mx
+const NAMELIST_HEADS = Dict{String,Any}(
+    "CONTROL" => ControlNamelist,
+    "SYSTEM" => SystemNamelist,
+    "ELECTRONS" => ElectronsNamelist,
+    "CELL" => CellNamelist,
+    "IONS" => IonsNamelist
+)
 
-function findnamelists(str::AbstractString)
-    captured = map(x -> x.captures, eachmatch(NAMELIST_BLOCK, str))
-    dict = Dict{Symbol,String}()
-    for (name, content) in captured
-        @match uppercase(name) begin
-            "CONTROL" => push!(dict, :ControlNamelist => content)
-            "SYSTEM" => push!(dict, :SystemNamelist => content)
-            "ELECTRONS" => push!(dict, :ElectronsNamelist => content)
-            "CELL" => push!(dict, :CellNamelist => content)
-            "IONS" => push!(dict, :IonsNamelist => content)
+function Base.parse(::Type{Namelist}, str::AbstractString)
+    namelists = Namelist[]
+    for nml in eachmatch(NAMELIST_BLOCK, str)
+        head, body = nml.captures
+        T = NAMELIST_HEADS[uppercase(head)]
+        result = Dict{Symbol,Any}()
+        for m in eachmatch(NAMELIST_ITEM, body)
+            k = Symbol(m[:key])
+            v = FortranData(string(m[:value]))
+            # Parse a `FortranData` from `value` as type of the field of the namelist `T`
+            if isnothing(m[:index])  # Cases like `ntyp = 2`
+                result[k] = parse(fieldtype(T, k), v)
+            else  # An entry with multiple values, e.g., `celldm(2) = 3.0`.
+                if m[:kind] == '('
+                    i = parse(Int, m[:index])
+                    v = parse(Float64, v)  # TODO: This is tricky.
+                    result[k] = if haskey(result, k)
+                        # If `celldm` occurs before, push the new value, else create a vector of pairs.
+                        fillbyindex!(result[k], i, v)
+                    else
+                        fillbyindex!([], i, v)
+                    end
+                else  # m[:kind] == '%'
+                    i = string(m[:index])
+                    # TODO: This is not finished!
+                end
+            end
         end
+        push!(namelists, T(T(), result))
     end
-    return dict
-end # function findnamelists
-
-function lexnamelist(content::AbstractString)
-    captured = map(x -> x.captures, eachmatch(NAMELIST_ITEM, content))
-    dict = Dict{String,Any}()
-    for (key, value) in captured
-        dict[key] = value
-    end
-    return dict
-end # function lexnamelist
-
-function Base.parse(T::Type{Namelist}, str::AbstractString)
-    dict = findnamelists(str)
-    return [parse(eval(key), value) for (key, value) in dict]
+    return namelists
 end # function Base.parse
-function Base.parse(T::Type{<:Namelist}, content::AbstractString)
-    regex = r"([\w\d]+)(?:\((\d+)\))?"
-    dict = lexnamelist(content)
-    result = Dict{Symbol,Any}()
-    for (key, value) in dict
-        captures = match(regex, key).captures
-        k = Symbol(string(captures[1]))
-        v = FortranData(string(value))
-        # We need to parse a `FortranData` from `value` as type of the field of the namelist `T`.
-        if !isnothing(captures[2])  # An entry with multiple values, e.g., `celldm(2) = 3.0`.
-            # If `celldm` occurs before, push the new value, else create a vector of pairs.
-            i = parse(Int, captures[2])
-            v = parse(typeintersect(eltype(fieldtype(T, k)), Union{Int,Float64}), v)
-            result[k] = (haskey(result, k) ? fillbyindex!(result[k], i, v) :
-                         fillbyindex!([], i, v))
-        else  # Cases like `ntyp = 2`
-            result[k] = parse(fieldtype(T, k), v)
-        end
-    end
-    return T(T(), result)
+function Base.parse(T::Type{<:Namelist}, str::AbstractString)
+    return filter(x -> isa(x, T), parse(Namelist, str))
 end # function parsenamelist
 
 function fillbyindex!(x::AbstractVector, index::Int, value::T) where {T}
