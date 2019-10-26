73fe83fd42539871681d51e223965312a68837c0
Qi Zhang <singularitti@outlook.com>
2019-10-26 16:49:26 -0400

    style: Reformat code

--- test/InputParsers/Namelists.jl
+++ test/InputParsers/Namelists.jl
@@ -15,7 +15,10 @@ using QuantumESPRESSOParsers.InputParsers.Namelists
     @test isnothing(parse(PWscf.ControlNamelist, "&control/"))
     @test parse(PWscf.ControlNamelist, "&control\n/\n") == PWscf.ControlNamelist()
     @test_logs((:info, "Namelist not found in string!"), parse(PWscf.ControlNamelist, " "))
-    @test_logs((:info, "Namelist found, but it is empty! Default values will be used!"), parse(PWscf.ControlNamelist, "&control\n/"))
+    @test_logs(
+        (:info, "Namelist found, but it is empty! Default values will be used!"),
+        parse(PWscf.ControlNamelist, "&control\n/"),
+    )
 end # testset
 
 @testset "Parse CP input" begin
