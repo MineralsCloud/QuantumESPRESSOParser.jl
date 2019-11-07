c271fd4444382477abb6ffb02c9d26ba990d3c94
Qi Zhang <singularitti@outlook.com>
2019-11-07 00:47:30 -0500

    :fire: Deprecate src/prelude.jl

--- src/QuantumESPRESSOParsers.jl
+++ src/QuantumESPRESSOParsers.jl
@@ -2,7 +2,23 @@ module QuantumESPRESSOParsers
 
 export InvalidUserInput, SubroutineError, ParseFailure
 
-include("prelude.jl")
+struct InvalidUserInput
+    msg::String
+end
+
+struct SubroutineError
+    name::String
+    cerr::String
+    msg::String
+end
+
+struct ParseFailure <: Exception
+    msg::String
+end
+
+# Referenced from https://discourse.julialang.org/t/how-to-get-the-non-nothing-type-from-union-t-nothing/30523
+nonnothingtype(::Type{T}) where {T} = Core.Compiler.typesubtract(T, Nothing)  # Should not be exported
+
 include("InputParsers/InputParsers.jl")
 include("OutputParsers/OutputParsers.jl")
 
