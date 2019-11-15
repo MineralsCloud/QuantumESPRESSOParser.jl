71e549258d28df1974ebcce46311a2e1c4802954
Qi Zhang <singularitti@outlook.com>
2019-11-15 03:39:17 -0500

    :art: Use `titleof` to simplify code
    after commit
    https://github.com/MineralsCloud/QuantumESPRESSOBase.jl/commit/578a48e0797bac08a069f0e7fa6dc39a32c21e9f

--- src/Namelists.jl
+++ src/Namelists.jl
@@ -13,6 +13,7 @@ module Namelists
 
 using Compat: isnothing
 using Fortran90Namelists.FortranToJulia: FortranData
+using QuantumESPRESSOBase: titleof
 using QuantumESPRESSOBase.Namelists: Namelist
 
 using QuantumESPRESSOParsers
@@ -24,23 +25,11 @@ const NAMELIST_ITEM = r"""
                       [ \t]* (?<value>\S+?) [ \t]*  # Match and store value
                       [\n,]                          # Return or comma separates "key = value" pairs
                       """mx
-const NAMELIST_HEADS = Dict{Symbol,String}(
-    :ControlNamelist => "CONTROL",
-    :SystemNamelist => "SYSTEM",
-    :ElectronsNamelist => "ELECTRONS",
-    :CellNamelist => "CELL",
-    :IonsNamelist => "IONS",
-    :WannierNamelist => "WANNIER",
-    :PHNamelist => "INPUTPH",
-    :Q2RNamelist => "INPUT",
-    :MatdynNamelist => "INPUT",
-    :DynmatNamelist => "INPUT",
-)
 
 # This is an internal function and should not be exported.
 function tryparse_internal(::Type{T}, str::AbstractString, raise::Bool) where {T<:Namelist}
     result = Dict{Symbol,Any}()
-    head = NAMELIST_HEADS[nameof(T)]
+    head = titleof(T)
     # From https://github.com/aiidateam/qe-tools/blob/570a648/qe_tools/parsers/qeinputparser.py#L305-L312
     NAMELIST_BLOCK = Regex("""
                            ^ [ \\t]* &$head [ \\t]* \$  # Match `Namelist`'s name
