a21e74de13abca1e1f70f0d371041bf3cf8f6257
Qi Zhang <singularitti@outlook.com>
2019-12-19 15:56:26 -0500

    :green_heart: Update docs build config

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,19 @@
-using Documenter, QuantumESPRESSOParsers
+using QuantumESPRESSOParsers
+using Documenter
 
 makedocs(;
     modules=[QuantumESPRESSOParsers],
-    format=Documenter.HTML(),
+    authors="Qi Zhang <singularitti@outlook.com>",
+    repo="https://github.com/MineralsCloud/QuantumESPRESSOParsers.jl/blob/{commit}{path}#L{line}",
+    sitename="QuantumESPRESSOParsers.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://MineralsCloud.github.io/QuantumESPRESSOParsers.jl",
+        assets=String[],
+    ),
     pages=[
         "Home" => "index.md",
     ],
-    repo="https://github.com/MineralsCloud/QuantumESPRESSOParsers.jl/blob/{commit}{path}#L{line}",
-    sitename="QuantumESPRESSOParsers.jl",
-    authors="Qi Zhang <singularitti@outlook.com>",
-    assets=String[],
 )
 
 deploydocs(;
