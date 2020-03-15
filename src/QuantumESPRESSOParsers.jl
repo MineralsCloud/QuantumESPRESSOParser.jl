8f08ac5a3f6a4d5933596f4046b577d1544a5523
Qi Zhang <singularitti@outlook.com>
2020-03-15 18:41:22 -0400

    Fix inclusions

--- src/QuantumESPRESSOParsers.jl
+++ src/QuantumESPRESSOParsers.jl
@@ -15,8 +15,6 @@ end
 # Referenced from https://discourse.julialang.org/t/how-to-get-the-non-nothing-type-from-union-t-nothing/30523
 nonnothingtype(::Type{T}) where {T} = Core.Compiler.typesubtract(T, Nothing)  # Should not be exported
 
-include("Namelists.jl")
-include("Cards/Cards.jl")
 include("Inputs/Inputs.jl")
 include("Outputs/Outputs.jl")
 
