8a34bffe0f5ff8443c06470af1fe34111d3bdb3d
Qi Zhang <singularitti@outlook.com>
2020-04-05 20:40:17 -0400

    Add type `OutputFile`

--- src/Outputs/Outputs.jl
+++ src/Outputs/Outputs.jl
@@ -11,13 +11,19 @@ julia>
 """
 module Outputs
 
-export SubroutineError
+export SubroutineError, OutputFile
 
 struct SubroutineError
     name::String
     cerr::String
     msg::String
 end
+
+struct OutputFile{A}
+    source::A
+end
+
+Base.read(f::OutputFile{String}) = read(f.source, String)
 
 include("PWscf.jl")
 
