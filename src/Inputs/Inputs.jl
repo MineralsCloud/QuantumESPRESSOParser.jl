3bd4f33c65deb5870c12df8ac1977952b6dfd7ed
Qi Zhang <singularitti@outlook.com>
2020-04-05 20:27:46 -0400

    Deprecate `InputString`, use `InputFile`

--- src/Inputs/Inputs.jl
+++ src/Inputs/Inputs.jl
@@ -16,30 +16,32 @@ using FilePaths: AbstractPath
 using PyFortran90Namelists: FortranData, Parser
 using QuantumESPRESSOBase.Inputs: Namelist, titleof
 
+export InputFile
+
 struct InvalidInput
     msg::String
 end
 
-struct InputString <: AbstractString
-    str::String
+struct InputFile{A}
+    source::A
 end
 
-Base.tryparse(::Type{T}, str::AbstractString) where {T<:Namelist} =
-    tryparse(T, InputString(str))
-function Base.tryparse(::Type{T}, str::InputString) where {T<:Namelist}
-    str = str.str
+function Base.tryparse(::Type{T}, str::AbstractString) where {T<:Namelist}
     d::Dict{String,Any} = Parser().reads(str)
     return if haskey(d, lowercase(titleof(T)))
         dict = Dict(Symbol(k) => v for (k, v) in d[lowercase(titleof(T))])
         T(; dict...)
     end
 end # function Base.tryparse
+Base.tryparse(::Type{T}, f::InputFile) where {T<:Namelist} = tryparse(T, read(f))
 
 function Base.parse(::Type{T}, str::AbstractString) where {T<:Namelist}
     x = tryparse(T, str)
     isnothing(x) ? throw(Meta.ParseError("cannot find namelist `$(titleof(T))`!")) : x
 end # function Base.parse
-Base.parse(::Type{T}, fp::AbstractPath) where {T<:Namelist} = parse(T, read(fp, String))
+Base.parse(::Type{T}, f::InputFile) where {T<:Namelist} = parse(T, read(f))
+
+Base.read(f::InputFile{String}) = read(f.source, String)
 
 include("PWscf.jl")
 # include("PHonon.jl")
