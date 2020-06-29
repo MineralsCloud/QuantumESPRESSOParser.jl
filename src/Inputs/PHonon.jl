1d6f2f3f96b60936c1853f222164b3f366f82471
singularitti <singularitti@outlook.com>
2020-06-29 09:01:41 -0400

    Uncomment `parse` in src/Inputs/PHonon.jl

--- src/Inputs/PHonon.jl
+++ src/Inputs/PHonon.jl
@@ -2,7 +2,17 @@ module PHonon
 
 using Compat: only
 using QuantumESPRESSOBase.Inputs: Namelist
-using QuantumESPRESSOBase.Inputs.PHonon
+using QuantumESPRESSOBase.Inputs.PHonon:
+    SpecialKPoint,
+    QPointsCard,
+    PhInput,
+    Q2rInput,
+    DynmatInput,
+    MatdynInput,
+    PhNamelist,
+    Q2rNamelist,
+    MatdynNamelist,
+    DynmatNamelist
 
 using PyFortran90Namelists: FortranData
 
@@ -15,7 +25,6 @@ const Q_POINTS_SPECIAL_BLOCK_REGEX = r"""
  )+
 )
 """imx
-
 const Q_POINTS_SPECIAL_ITEM_REGEX = r"""
 ^ [ \t]*
 ([\-|\+]?(?:\d*[\.]\d+ | \d+[\.]?\d*)
@@ -28,41 +37,32 @@ const Q_POINTS_SPECIAL_ITEM_REGEX = r"""
     (?:[E|e|d|D][+|-]?\d+)?)\h*
 """imx
 
-# function Base.parse(::Type{QPointsSpecsCard}, str::AbstractString)
-#     m = match(Q_POINTS_SPECIAL_BLOCK_REGEX, str)
-#     if m !== nothing
-#         captured = only(m.captures)
-#         data = SpecialQPoint[]
-#         for matched in eachmatch(Q_POINTS_SPECIAL_ITEM_REGEX, captured)
-#             # TODO: Match `nqs`
-#             point = SpecialQPoint(map(x -> parse(Float64, FortranData(x)), matched.captures)...)
-#             push!(data, point)
-#         end
-#         return QPointsSpecsCard(data)
-#     end
-#     @info "Cannot find card `Q_POINTS`!"
-# end # function Base.parse
-
-# function Base.parse(::Type{T}, str::AbstractString) where {T<:PhInput}
-#     inputph = parse(PhNamelist, str)
-#     q_points = parse(QPointsSpecsCard, str)
-#     return T(inputph, q_points)
-# end # function Base.parse
-
-function Base.parse(::Type{T}, str::AbstractString) where {T<:Q2rInput}
-    input = parse(Q2rNamelist, str)
-    return T(input)
+function Base.parse(::Type{QPointsCard}, str::AbstractString)
+    m = match(Q_POINTS_SPECIAL_BLOCK_REGEX, str)
+    if m === nothing
+        @info "no q-points are provided."
+        return
+    else
+        captured = only(m.captures)
+        data = map(eachmatch(Q_POINTS_SPECIAL_ITEM_REGEX, captured)) do matched
+            # TODO: Match `nqs`
+            SpecialKPoint(map(x -> parse(Float64, FortranData(x)), matched.captures))
+        end
+        return QPointsCard(data)
+    end
 end # function Base.parse
-
-# function Base.parse(::Type{T}, str::AbstractString) where {T<:MatdynInput}
-#     input = parse(MatdynNamelist, str)
-#     q_points = parse(QPointsSpecsCard, str)
-#     return T(input, q_points)
-# end # function Base.parse
-
-function Base.parse(::Type{T}, str::AbstractString) where {T<:DynmatInput}
-    input = parse(DynmatNamelist, str)
-    return T(input)
+function Base.parse(::Type{PhInput}, str::AbstractString)
+    inputph = parse(PhNamelist, str)
+    q_points = parse(QPointsCard, str)
+    return PhInput(inputph, q_points)
 end # function Base.parse
+Base.parse(::Type{Q2rInput}, str::AbstractString) = Q2rInput(parse(Q2rNamelist, str))
+function Base.parse(::Type{MatdynInput}, str::AbstractString)
+    input = parse(MatdynNamelist, str)
+    q_points = parse(QPointsCard, str)
+    return MatdynInput(input, q_points)
+end # function Base.parse
+Base.parse(::Type{DynmatInput}, str::AbstractString) =
+    DynmatInput(parse(DynmatNamelist, str))
 
 end
