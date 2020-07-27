f75bb1b514bc15355ea10b05170c47012ac773a6
singularitti <singularitti@outlook.com>
2020-07-27 01:40:21 -0400

    Deprecate `InputFile` type

--- src/Inputs/Inputs.jl
+++ src/Inputs/Inputs.jl
@@ -15,18 +15,10 @@ using PyFortran90Namelists: FortranData, Parser
 using QuantumESPRESSOBase.Inputs:
     Namelist, QuantumESPRESSOInputEntry, Input, titleof, inputstring
 
-export InputFile
-
 struct InvalidInput
     msg::String
 end
 
-struct InputFile{A}
-    source::A
-end
-
-Base.tryparse(::Type{T}, f::InputFile) where {T<:QuantumESPRESSOInputEntry} =
-    tryparse(T, read(f))
 function Base.tryparse(::Type{T}, str::AbstractString) where {T<:Namelist}
     d::Dict{String,Any} = Parser().reads(str)
     return if haskey(d, lowercase(titleof(T)))
@@ -35,7 +27,6 @@ function Base.tryparse(::Type{T}, str::AbstractString) where {T<:Namelist}
     end
 end # function Base.tryparse
 
-Base.parse(::Type{T}, f::InputFile) where {T<:QuantumESPRESSOInputEntry} = parse(T, read(f))
 function Base.parse(::Type{T}, str::AbstractString) where {T<:Namelist}
     x = tryparse(T, str)
     if x === nothing
@@ -44,12 +35,6 @@ function Base.parse(::Type{T}, str::AbstractString) where {T<:Namelist}
         return x
     end
 end # function Base.parse
-
-Base.read(f::InputFile{String}) = read(f.source, String)
-
-Base.write(f::InputFile{String}, x::QuantumESPRESSOInputEntry) =
-    write(f.source, inputstring(x))
-Base.write(f::InputFile{String}, x::Input) = write(f.source, inputstring(x))
 
 include("PWscf.jl")
 include("PHonon.jl")
