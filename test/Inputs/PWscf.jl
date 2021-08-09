814a813db9ae9e25eb1e0cbfb2e99a9643401a8d
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-09 18:44:14 -0400

    Add test "Test parsing a `PWInput`: silicon bands"

--- test/Inputs/PWscf.jl
+++ test/Inputs/PWscf.jl
@@ -49,3 +49,62 @@ end
     )
     @test pw.cell_parameters === nothing
 end
+
+@testset "Test parsing a `PWInput`: silicon bands" begin
+    # From https://github.com/QEF/q-e/blob/121edf5/PW/examples/example01/run_example#L125-L172
+    str = read("data/si.band.cg.in", String)
+    pw = parse(PWInput, str)
+    @test pw.control == ControlNamelist(
+        calculation = "bands",
+        pseudo_dir = raw"pseudo/",
+        outdir = raw"./",
+        prefix = "silicon",
+    )
+    @test pw.system == SystemNamelist(
+        ibrav = 2,
+        celldm = [10.2],
+        nat = 2,
+        ntyp = 1,
+        ecutwfc = 18.0,
+        nbnd = 8,
+    )
+    @test pw.electrons == ElectronsNamelist(; diagonalization = "cg")
+    @test pw.atomic_species ==
+          AtomicSpeciesCard([AtomicSpecies("Si", 28.086, "Si.pz-vbc.UPF")])
+    @test pw.atomic_positions == AtomicPositionsCard([
+        AtomicPosition("Si", [0.0, 0.0, 0.0]),
+        AtomicPosition("Si", [0.25, 0.25, 0.25]),
+    ])
+    @test pw.k_points == SpecialPointsCard(
+        [
+            0.0 0.0 0.0 1.0
+            0.0 0.0 0.1 1.0
+            0.0 0.0 0.2 1.0
+            0.0 0.0 0.3 1.0
+            0.0 0.0 0.4 1.0
+            0.0 0.0 0.5 1.0
+            0.0 0.0 0.6 1.0
+            0.0 0.0 0.7 1.0
+            0.0 0.0 0.8 1.0
+            0.0 0.0 0.9 1.0
+            0.0 0.0 1.0 1.0
+            0.0 0.0 0.0 1.0
+            0.0 0.1 0.1 1.0
+            0.0 0.2 0.2 1.0
+            0.0 0.3 0.3 1.0
+            0.0 0.4 0.4 1.0
+            0.0 0.5 0.5 1.0
+            0.0 0.6 0.6 1.0
+            0.0 0.7 0.7 1.0
+            0.0 0.8 0.8 1.0
+            0.0 0.9 0.9 1.0
+            0.0 1.0 1.0 1.0
+            0.0 0.0 0.0 1.0
+            0.1 0.1 0.1 1.0
+            0.2 0.2 0.2 1.0
+            0.3 0.3 0.3 1.0
+            0.4 0.4 0.4 1.0
+            0.5 0.5 0.5 1.0
+        ],
+    )
+end
