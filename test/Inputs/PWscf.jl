719b22177868563072240e890ba69384b8098558
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-09 18:23:19 -0400

    Remove a test

--- test/Inputs/PWscf.jl
+++ test/Inputs/PWscf.jl
@@ -8,7 +8,6 @@ using QuantumESPRESSOParser.Inputs
 @testset "Parse empty strings" begin
     @test_throws Meta.ParseError parse(ControlNamelist, " ")
     @test tryparse(ControlNamelist, " ") === nothing
-    @test_throws Meta.ParseError parse(ControlNamelist, "&control/")
     @test tryparse(ControlNamelist, "&control/") == ControlNamelist()
     @test parse(ControlNamelist, "&control\n/\n") == ControlNamelist()
     @test_throws Meta.ParseError parse(ControlNamelist, " ")
