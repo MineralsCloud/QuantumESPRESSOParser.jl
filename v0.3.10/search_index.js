fb18b76e1acc1eb2e18d90209e2810b68d70819c
Documenter.jl <documenter@juliadocs.github.io>
2022-05-30 08:20:31 +0000

    build based on 898394d

---
+++ v0.3.10/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QuantumESPRESSOParser","category":"page"},{"location":"#QuantumESPRESSOParser","page":"Home","title":"QuantumESPRESSOParser","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuantumESPRESSOParser.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QuantumESPRESSOParser]","category":"page"}]
+}
