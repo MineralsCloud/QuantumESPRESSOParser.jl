8fba944ac382abea7bc787493a691f90870dffc3
singularitti <singularitti@outlook.com>
2023-10-28 00:47:28 -0400

    Move regexes `CELL_PARAMETERS_BLOCK_OUTPUT`, etc. to src/PWscf/output/atomicstructure.jl

--- src/PWscf/output/regexes.jl
+++ src/PWscf/output/regexes.jl
@@ -73,56 +73,6 @@ number of k points=\s*(?<nk>[0-9]+)\h*(?<metainfo>.*)
 const K_POINTS_ITEM = Regex(
     "k\\(.*\\) = \\(\\s*$FIXED_POINT_REAL\\s*$FIXED_POINT_REAL\\s*$FIXED_POINT_REAL\\s*\\), wk =\\s*$FIXED_POINT_REAL",
 )
-# The following format is from https://github.com/QEF/q-e/blob/4132a64/PW/src/output_tau.f90#L47-L60.
-const CELL_PARAMETERS_BLOCK_OUTPUT = r"""
-CELL_PARAMETERS \h+
-\( (?<option>\w+) =? \s* (?<alat>[-+]?[0-9]*\.[0-9]{8})? \) \h*  # Match `alat`: `F12.8`
-(?<data>
-    (?: \s*
-        (?:
-            [-+]?[0-9]*\.[0-9]+ \s*  # Match element
-        ){3}  # I need exactly 3 elements per vector
-    ){3}  # I need exactly 3 vectors
-)
-"""x
-const CELL_PARAMETERS_ITEM_OUTPUT = r"""
-\s*
-([-+]?[0-9]*\.[0-9]+) \s*  # x
-([-+]?[0-9]*\.[0-9]+) \s*  # y
-([-+]?[0-9]*\.[0-9]+) \s*  # z
-"""x
-# The following format is from https://github.com/QEF/q-e/blob/4132a64/PW/src/output_tau.f90#L64-L109.
-const ATOMIC_POSITIONS_BLOCK_OUTPUT = r"""
-ATOMIC_POSITIONS \h*                   # Atomic positions start with that string
-\( (?<option>\w+) \)                   # Option of the card
-(?<data>
-    (?:
-        \s*
-        [A-Za-z]+[A-Za-z0-9]{0,2} \s+  # Atom spec
-        (?:
-            [-+]?[0-9]*\.[0-9]+ \s*  # Match element
-        ){3}                           # I need exactly 3 floats per vector.
-        (?:
-            [-+]?[0-9]+ \s*
-        ){0,3}                     # I need exactly 3 integers in `if_pos`, if there is any.
-    )+
-)
-"""x
-const ATOMIC_POSITIONS_ITEM_OUTPUT = r"""
-\s*
-([A-Za-z]+[A-Za-z0-9]{0,2}) \s+  # Atom spec
-([-+]?[0-9]*\.[0-9]+) \s*  # x
-([-+]?[0-9]*\.[0-9]+) \s*  # y
-([-+]?[0-9]*\.[0-9]+) \s*  # z
-([-+]?[0-9]+)? \s*            # if_pos(1)
-([-+]?[0-9]+)? \s*            # if_pos(2)
-([-+]?[0-9]+)? \s*            # if_pos(3)
-"""x
-const FINAL_COORDINATES_BLOCK = r"""
-Begin final coordinates
-(\X+?)
-End final coordinates
-"""
 const STRESS_BLOCK = r"""
 ^[ \t]*
 total\s+stress\s*\(Ry\/bohr\*\*3\)\s+
